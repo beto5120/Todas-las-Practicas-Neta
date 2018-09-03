897e365ff3e436e0aca71bf2e9a3e34198b9c98b
NS-388 <NS-388@DESKTOP-B1IG530>
2018-09-03 14:53:44 -0500

    hola-web

---
+++ hola-web/src/main/java/net/netasystems/servlets/HolaMundo.java
@@ -0,0 +1,50 @@
+package net.netasystems.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaMundo
+ */
+public class HolaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HolaMundo() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<html>");
+		response.getWriter().append("<body>");
+		response.getWriter().append("<h1>");
+		response.getWriter().append("<Hola mundo desde servlet!>");
+		for(int i = 0; i < 10; i++) {
+			
+			
+		}
+		
+		
+		response.getWriter().append("</h1>");
+		response.getWriter().append("</body>");
+		response.getWriter().append("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
