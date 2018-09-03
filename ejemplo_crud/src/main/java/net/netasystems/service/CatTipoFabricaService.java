8dbae678c5aa14d8b091df1fffb969c3faea4c15
NS-388 <NS-388@DESKTOP-B1IG530>
2018-09-03 14:35:42 -0500

    crud

---
+++ ejemplo_crud/src/main/java/net/netasystems/service/CatTipoFabricaService.java
@@ -0,0 +1,25 @@
+package net.netasystems.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import net.netasystems.dao.CatTipoFabricaDAO;
+import net.netasystems.domain.CatTipoFabrica;
+
+public class CatTipoFabricaService {
+
+	private CatTipoFabricaDAO dao;
+	
+	public CatTipoFabricaService() throws ClassNotFoundException, SQLException {
+		dao = new CatTipoFabricaDAO();
+	}
+	
+	public void close() throws SQLException {
+		dao.destroy();
+	}
+	
+	public List<CatTipoFabrica> getAllRecords() throws SQLException{
+		return dao.getAllRecords();
+	}
+	
+}
